deffe32614a84680648c812a4f660dd70f8a4611
minhyeoklee <minhyeok.lee1@gmail.com>
2022-01-07 09:07:33 +0900

    'readme'

--- nextjs-blog/README.md
+++ nextjs-blog/README.md
@@ -1,1 +1,11 @@
+* [1월 3주차 OJT KEYWORD: Next.js Dir(dir)](https://github.com/MinHyeok-lee1/MK_OJT/tree/master/nextjs-dir)
+1) nextjs.org 튜토리얼 클론 코딩 - 로컬 서버의 개인 작업공간 이용<br>
+2) Git을 통해 소스코드 관리<br>
+3) 학습 내용을 소스코드의 주석 및 README.md 파일로 정리<br><br>
+
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+
+<a href="/">Back to home</a>
+
+<Link href="/"><a>Back to home</a></Link>
--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,3 +1,20 @@
+import Link from 'next/link'
+
 export default function FirstPost() {
-  return <h1>First Post</h1>
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+
+      <Link href="/">
+      <a className="foo" target="_blank" rel="noopener noreferrer">
+        Hello World
+      </a>
+    </Link>
+    </>
+  )
 }
