b2e10e3938564dff127d4f337042e8f076a73510
minhyeoklee <minhyeok.lee1@gmail.com>
2022-02-07 16:03:32 +0900

    a

--- 03_Toy_Project/back-end/src/app.module.ts
+++ 03_Toy_Project/back-end/src/app.module.ts
@@ -11,9 +11,17 @@ import {
   Mongoose
 } from 'mongoose';
 
+
+const uri = "mongodb+srv://kfdd6630:C6xjaUX2e7TVkW!@cluster0.exm9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 @Module({
   imports: [ToysModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/test')
+    MongooseModule.forRoot('mongodb://localhost:27017/test', {
+      connectionName: 'cats',
+    }),
+    MongooseModule.forRoot(uri, {
+      connectionName: 'dog',
+    }),
   ],
   controllers: [],
   providers: [],
--- 03_Toy_Project/back-end/src/main.ts
+++ 03_Toy_Project/back-end/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // Swagger 문서툴 설정
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('API server')
+  .setDescription("The API server's API description")
+  .setVersion('1.0')
+  .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
